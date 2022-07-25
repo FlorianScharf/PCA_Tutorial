07d865eeaef55fc3429d47e03cd535d525c8cafd
Andreas Widmann <widmann@uni-leipzig.de>
2022-07-25 20:10:14 +0200

    Added documentation.

--- tools/fig10_eeglab.m
+++ tools/fig10_eeglab.m
@@ -3,6 +3,23 @@ function fig10_eeglab
 % This script enables visual inspection of the factors and reproduces Fig. 10 in MATLAB/EEGLAB
 % Author: Andreas Widmann, widmann@uni-leipzig.de
 % Copyright (c) 2022 Andreas Widmann, University of Leipzig
+%
+% Required steps:
+%   (1) Adjust filename for EEGLAB template (or fill required values
+%       manually).
+%   (2) Adjust filename for individual average file (the same PCA was
+%       computed on). Complete dimensions in case necessary.
+%   (3) Adjust filename for PCA solution exported from R.
+%   (4) Reshape and permute individual average matrix. This step is
+%       essential and heavily dependent on the format (row order, cf.
+%       Fig. 3 "Observations") of the input data. Target order of matrix
+%       dimensions is: chan x time x cond x subj.
+%   (5) Reshape and permute PCA scores matrix. This step is essential and
+%       heavily dependent on the format (row order, cf. Fig. 3
+%       "Observations") of the input data. Target order of matrix
+%       dimensions is: chan x time x cond x subj x fac.
+%   (6) Select conditions to plot (delete other) and compute difference
+%       waves in case necessary.
 
 % Basic figure options
 figname = 'adPCA_eeglab-1';
@@ -10,7 +27,9 @@ figname = 'adPCA_eeglab-1';
 % Optional plot_pcacmp arguments; uncomment and adapt
 figOpts = {};
 figOpts = [ figOpts, { 'condlabels' }, { { { 'sta', 'nov', 'nov-sta' } } } ]; % Condition labels for legend and topography headings
+% figOpts = [ figOpts, { 'condlabels' }, { { { 'nov', 'nov-sta' } } } ]; % Condition labels for legend and topography headings
 figOpts = [ figOpts, { 'factors' },  { 1:6 } ]; % Which factors to plot
+figOpts = [ figOpts, { 'legend' }, { 'on' } ]; % Show legend (on/off)
 % figOpts = [ figOpts, { 'colororder' }, { get( 0, 'DefaultAxesColorOrder' ) } ]; % Custom line colors
 % figOpts = [ figOpts, { 'alpha' }, { 0.4 } ]; % Grand-average line transparency; note that MATLAB currently does NOT support alpha adjustment for lines and this feature is implemented by color adjustment
 % figOpts = [ figOpts, { 'rois' }, { { { { 'F3', 'Fz', 'F4' }, { 'FC1', 'Cz', 'FC2' }, { 'Fz', 'FC1', 'FC2', 'Cz' }, { 'F3', 'Fz', 'F4' }, { 'Cz' }, { 'F3', 'Fz', 'F4' } } } } ]; % ROIs to plot for each factor; default abs factor peak electrode location
@@ -19,7 +38,7 @@ figOpts = [ figOpts, { 'factors' },  { 1:6 } ]; % Which factors to plot
 try colormap = brewermap(64, 'RdBu'); colormap = flipud( colormap ); catch colormap = 'jet'; end
 figOpts = [ figOpts, { 'colormap' }, { colormap } ];
 
-% Load an EEGLAB data file for channel locations and some required parameters
+% (1) Load an EEGLAB data file for channel locations and some required parameters
 TMP = pop_loadset( 'filepath', '../data', 'filename', 'ad-sta.set' );
 EEG = eeg_emptyset;
 EEG.srate = TMP.srate;
@@ -30,7 +49,7 @@ EEG.pnts = TMP.pnts;
 EEG.times = ( ( 0:EEG.pnts - 1 ) / EEG.srate + EEG.xmin ) * 1000;
 EEG.xmax = max( EEG.times / 1000 );
 
-% Load individual averages
+% (2) Load individual averages
 load( '../data/avrdata.mat', 'data', 'dataIdx' )
 data = data( dataIdx( :, 1 ) == 1, : ); % Plot adult group only
 dataIdx = dataIdx( dataIdx( :, 1 ) == 1, : ); % Plot adult group only
@@ -38,22 +57,23 @@ nCond = 2; % 1 = sta, 2 = dev
 nSubj = length( unique( dataIdx( :, 3 ) ) );
 nChan = length( unique( dataIdx( :, 4 ) ) );
 
-% Load rotated PCA solution
+% (3) Load rotated PCA solution
 load( '../results/02bc_rotation_score/rotfit_ad23_geomin0.01.mat' )
 nFac = size( loadings, 2 );
 
-% Reshape PCA data
+% (4) Reshape individual averages
+data = reshape( data, nChan, nSubj, nCond, EEG.pnts ); % chan x subj x cond x time; depends on order in exported data matrix!
+data = permute( data, [ 1 4 3 2 ] ); % chan x time x cond x subj
+
+% (5) Reshape PCA data
 EPdata.data = reshape( scores, nChan, nSubj, nCond, nFac ); % chan x subj x cond x fac; depends on order in exported data matrix!
 EPdata.data = permute( EPdata.data, [ 1 5 3 2 4 ] ); % chan x time x cond x subj x fac;
 EPdata.facVecT = diag(varSD) * loadings;
 
-% Reshape individual averages
-data = reshape( data, nChan, nSubj, nCond, EEG.pnts ); % chan x subj x cond x time; depends on order in exported data matrix!
-data = permute( data, [ 1 4 3 2 ] ); % chan x time x cond x subj
-
-% Difference waves
+% (6) Difference waves
 EPdata.data( :, :, 3, :, : ) = EPdata.data( :, :, 2, :, : ) - EPdata.data( :, :, 1, :, : );
 data( :, :, 3, : ) = data( :, :, 2, : ) - data( :, :, 1, : );
+% EPdata.data( :, :, 1, :, : ) = []; % Delete conditions which should not be plotted
 
 % Plot data
 plot_pcacmp( EPdata, data, EEG, figOpts{ : } )
